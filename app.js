ccf2a0a40bc0f7a46c2fe40450572cf12d713b07
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-11-01 01:17:01 +0600

    Animation

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const signinBtn = document.querySelector('.signinBtn');
 const signupBtn = document.querySelector('.signupBtn');
 const fromBx = document.querySelector('.fromBx');
+const body = document.querySelector('body');
 
 signupBtn.onclick = function(){
     fromBx.classList.add('active');
+    body.classList.add('active');
 }
 signinBtn.onclick = function(){
     fromBx.classList.remove('active');
+    body.classList.remove('active');
 }
