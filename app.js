7dff0d665c769169be1d545b1bad56291605254c
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-11-06 21:54:02 +0600

    Value Read

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const signupBtn = document.querySelector('.signupBtn');
 const fromBx = document.querySelector('.fromBx');
 const body = document.querySelector('body');
 
-//----
+//----Input Value Collect
+let name,email,password,confirmPassword;
+name = document.getElementById('name').Value;
+email = document.getElementById('email').Value;
+password = document.getElementById('password').Value;
+confirmPassword = document.getElementById('confirmPassword').Value;
+
+
 
 signupBtn.onclick = function(){
     fromBx.classList.add('active');
