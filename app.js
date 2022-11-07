d5227a19292c3db9d054927cdea5871cfcb9ec0b
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-11-07 08:47:02 +0600

    Value Read

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ const signupBtn = document.querySelector('.signupBtn');
 const fromBx = document.querySelector('.fromBx');
 const body = document.querySelector('body');
 
-//----Input Value Collect
-function save(){
-    let name,email,password,confirmPassword;
-    name = document.getElementById('name').value;
-    email = document.getElementById('email').value;
-    password = document.getElementById('password').value;
-    confirmPassword = document.getElementById('confirmPassword').value;
 
-//Set Value Local Storage
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
-    localStorage.setItem('password',password);
-    localStorage.setItem('confirmPassword',confirmPassword);
+// function save(){
+  
 
-//Clear Input Values
-    name.value='';
+// }
 
-}
+
+const saveBtn = document.getElementById('save_btn');
+saveBtn.addEventListener('click',function handlerClick(event){
+    event.preventDefault();
+
+    //----Input Value Collect
+      let name,email,password,confirmPassword;
+      name = document.getElementById('name').value;
+      email = document.getElementById('email').value;
+      password = document.getElementById('password').value;
+      confirmPassword = document.getElementById('confirmPassword').value;
+  
+    //Set Value Local Storage
+      localStorage.setItem('name',name);
+      localStorage.setItem('email',email);
+      localStorage.setItem('password',password);
+      localStorage.setItem('confirmPassword',confirmPassword);
+  
+    //Clear Input Values
+      name.value='';
+})
 
 
 
