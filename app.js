9af9f7757042178929404d07840d62557db0fd63
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-11-08 23:47:21 +0600

    Value Read

--- app.js
+++ app.js
@@ -28,10 +28,7 @@ saveBtn.addEventListener('click',function handlerClick(event){
       localStorage.setItem('password',password);
       localStorage.setItem('confirmPassword',confirmPassword);
 
-    // const inputs = document.querySelectorAll('#nameValue')
-    // inputs.forEach(input=>{
-    //     input.value='';
-    // })  
+    
   
     
 })
